6d9b978f09a2dee85556d356f8b0779e9df3f3e7
underr <underr-ua@users.noreply.github.com>
2016-11-09 16:12:15 +0200

    js upd

--- src/js/src/ui/ui_doc_main.js
+++ src/js/src/ui/ui_doc_main.js
@@ -4,23 +4,6 @@
 'use strict';
 
 
-const PHENOD_CMD = {
-    PING: 'ping',
-    LOGIN: 'login'
-};
-
-const IMG = {
-    LOGO_WAIT: 'img/logo_wt.gif',
-    LOGO_DONE: 'img/logo_dn.png',
-    LOGO_FAIL: 'img/logo_fl.png'
-};
-
-var g_img = [
-    IMG.LOGO_WAIT, IMG.LOGO_DONE, IMG.LOGO_FAIL
-];
-
-var g_img_preload = [];
-
 function doc_main_write() {
     document.write(main_write());
 }
