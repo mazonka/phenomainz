7e2024ae77637ddc5d6cc5173f217fb0fb99d412
underr <underr-ua@users.noreply.github.com>
2016-12-09 06:45:26 +0200

    js

--- src/js/src/ui/ui.js
+++ src/js/src/ui/ui.js
@@ -25,9 +25,14 @@ function ui_init(uid) {
     g_uid = uid;
     console.log(uid);
     
+    $('#td_profile').hide();
+    $('#td_open_file').hide();
+    $('#td_login').hide();
+
     $('button').button();
     $('#button_user_email').prop('disabled', true);
     $('#div_modal_window').css('display', 'none');
+    
     
     wid_nc_ping();
 }
