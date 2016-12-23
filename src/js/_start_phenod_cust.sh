888c176395dabedbc91e35fc9b987851ee383c38
underr <underr-ua@users.noreply.github.com>
2016-12-23 13:36:18 +0200

    js

---
+++ src/js/_start_phenod_cust.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+#
+# (C) 2016
+#
+
+PLAT=${PLAT:-msc}
+BIN=_bin_${PLAT}
+
+echo ${BIN}
+cwd=`pwd`
+test -f /usr/bin/cygpath && cwd=`cygpath -m $cwd`
+#comm="./phenod tcp_port=16000 dprn=1 dpul=1 dced=1 dwkr=1 cycle=10000"
+#comm="./phenod"
+comm="phenod skcseed=pheno dpul=0 s=$cwd/pheno.inp"
+
+error() {
+	[ -z "$1" ] || echo ">>> $1"
+	exit 1
+}
+
+
+[ "$?" -eq 0 ] || error
+
+cd "../$BIN/"
+
+if cmd /c ls 2> /dev/null 1> /dev/null
+then
+echo $comm > strt.bat
+echo pause >> strt.bat
+#cmd /c start $comm
+cmd /c start cmd /c strt.bat
+else
+./$comm
+fi
+
+
