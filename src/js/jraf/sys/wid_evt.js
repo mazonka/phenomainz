8475a1bc3b186037f77acb534d499aae3adc46f5
underr <underr-ua@users.noreply.github.com>
2017-01-20 11:15:49 +0200

    js upd

--- src/js/jraf/sys/wid_evt.js
+++ src/js/jraf/sys/wid_evt.js
@@ -13,48 +13,6 @@ function evt_keyup_esc(e)
     e.keyCode == 27 && wid_init_modal_window(false);
 }
 
-function evt_input_name($obj, node)
-{
-    var $div = $obj.parent();
-    var $btn = $div.find('button');
-    var $inp = $div.find('input');
-    var text = $inp
-        .val()
-        .replace(/^\s+|\s+$/g, '');
-    
-    if (eng_is_valid_str(text))
-    {
-        wid_paint_borders($inp);
-        
-        $btn
-            .off('click')
-            .button('enable')
-            .click(function()
-            {
-                jraf_write_name(node, text || '*');
-                wid_close_modal_window();
-            });
-        
-        $inp
-            .off('keyup')
-            .keyup(function(event)
-            {
-                if (event.keyCode === 13)
-                {
-                    jraf_write_name(node, text || '*');
-                    wid_close_modal_window();
-                }
-            });
-    }
-    else
-    {
-        wid_paint_borders($inp, 'red');
-        
-        $btn.button('disable');
-        $inp.off('keyup');
-    }
-}
-
 function evt_click_create_users_dir()
 {
     var dpath = '/.jraf.sys/users';
